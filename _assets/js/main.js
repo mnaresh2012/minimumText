33ecb7bbc0d228b1741aaae65fc427b2037bc1f5
naresh kumar <mnaresh2010@aol.com>
2016-06-30 17:47:09 +0530

    Update main.js

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,7 +5,7 @@ $(function() {
 	//pugin initialization
 	$('.container').minimumText({
 		element: '.jumbotron p.description',
-		numsChar: 10,
+		numsChar: 125,
 		url: 'www.google.com',
 		dots: '....',
 		str: 'view more'
